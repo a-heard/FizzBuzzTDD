de877886c41617b573fbb6ab54a5d4654391d07c
a-heard <62103675+a-heard@users.noreply.github.com>
2020-06-01 09:36:54 -0400

    process Number now returns any integer passed as itself as a string

---
+++ FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package FizzBuzz;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void test() {
+        
+    }
+
+    @Test
+    public void shouldProcessInput() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.processNumber(1));
+        assertEquals("2", fizzBuzz.processNumber(2));
+    }
+}
